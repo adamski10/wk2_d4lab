4a483f73a6bbddbeda9213c86488625689154c1e
Craig McLeod <mcleod.craig@outlook.com>
2020-05-21 14:59:07 +0100

    Test planet by name and planets with no moons

--- star_system.rb
+++ star_system.rb
@@ -1,3 +1,5 @@
+require ('pry')
+
 class StarSystem
   attr_reader :name, :planets
 
@@ -6,5 +8,25 @@ class StarSystem
     @planets = planets
   end
 
+  def planet_names
+    planet_names = @planets.map { |planet| planet.name }
+    return planet_names    
+  end
+
+  def get_planet_by_name(planet_name)
+    planet_by_name = @planets.find { |planet| planet.name == planet_name }
+    return planet_by_name
+  end
+  
+  def get_planets_with_no_moons
+    planets_without_moons = @planets.find_all { |planet| planet.number_of_moons == 0 }
+    return planets_without_moons    
+  end
     
 end
+
+# def test_get_planets_with_no_moons
+#   expected_planets = [ @mercury, @venus ]
+#   result = @solar_system.get_planets_with_no_moons
+#   assert_equal(expected_planets, result)
+# end
